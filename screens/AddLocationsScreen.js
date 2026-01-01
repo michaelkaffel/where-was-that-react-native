c75a3ff83b09658f6978130249f9507d2da345fc
MichaelKaffel <michaeldkaffel@gmail.com>
2026-01-01 15:41:09 -0800

    navigation set up for main sections of app

---
+++ screens/AddLocationsScreen.js
@@ -0,0 +1,18 @@
+import { Text } from "react-native-elements";
+
+const AddLocationsScreen = () => {
+    return (
+        <>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        <Text>AddLocationsScreen</Text>
+        </>
+    )
+};
+
+export default AddLocationsScreen;
---
+++ screens/OverlooksScreen.js
@@ -0,0 +1,20 @@
+import { Text } from "react-native-elements";
+
+const OverlooksScreen = () => {
+    return (
+        <>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+            <Text>OverlooksScreen</Text>
+        </>
+    )
+};
+
+export default OverlooksScreen;
