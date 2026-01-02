c498e374f11e25dbd988218b1a07c7a2e12ab461
MichaelKaffel <michaeldkaffel@gmail.com>
2026-01-01 18:37:48 -0800

    Navigation implemented for overlooks

--- screens/OverlooksScreen.js
+++ screens/OverlooksScreen.js
@@ -1,20 +1,35 @@
-import { Text } from "react-native-elements";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
+import { OVERLOOKS } from "../shared/OVERLOOKS";
 
-const OverlooksScreen = () => {
+const OverlooksScreen = ({ navigation }) => {
+    const [overlooks, setOverlooks] = useState(OVERLOOKS);
+
+    const renderOverlook = ({ item: overlook }) => {
+        return (
+            <ListItem
+                key={overlook.id}
+                onPress={() => 
+                    navigation.navigate('OverlookInfo', { overlook })
+                }
+            >
+                <Avatar source={overlook.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{overlook.title}</ListItem.Title>
+                    <ListItem.Subtitle>{overlook.location}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
+
     return (
-        <>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-            <Text>OverlooksScreen</Text>
-        </>
-    )
+        <FlatList 
+            data={overlooks}
+            renderItem={renderOverlook}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    );
 };
 
 export default OverlooksScreen;
---
+++ shared/OVERLOOKS.js
@@ -0,0 +1,35 @@
+
+
+
+export const OVERLOOKS = [
+    {
+        id: 0,
+        title: 'Blue Mountain',
+        description: 'Fairly steep and narrow dirt road up to an overlook, small hiking trail, and a campground.',
+        image: require('../assets/images/IMG_5617.jpeg'),
+        location: 'Port Angeles, WA',
+        dateVisited: '12/10/25',
+        favorite: false,
+        kindOfPlace: 'overlook'
+    },
+    {
+        id: 1,
+        title: 'Washington Pass',
+        description: 'Found on the way while driving out to Eastern Washington. Not worth the drive on it own but a good place to stop.',
+        image: require('../assets/images/washingtonpass.jpg'),
+        location: 'Gilbert, WA',
+        dateVisited: '12/10/25',
+        favorite: false,
+        kindOfPlace: 'overlook'
+    },
+    {
+        id: 2,
+        title: 'Hurricane Ridge',
+        description: 'Relatively short drive from Port Angeles. Central hub for othe roads, skiing, and hiking.',
+        image: require('../assets/images/IMG_4064.jpeg'),
+        location: 'Port Angeles, WA',
+        dateVisited: '12/10/25',
+        favorite: false,
+        kindOfPlace: 'overlook'
+    }
+]
