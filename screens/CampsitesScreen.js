62ca2836bea938964a010a3f1e94556c6617c6c6
MichaelKaffel <michaeldkaffel@gmail.com>
2026-01-14 16:03:55 -0800

    sketched out how favorites will look

--- screens/CampsitesScreen.js
+++ screens/CampsitesScreen.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 // import { useState } from "react";
 import { FlatList } from "react-native";
-import { Avatar, ListItem } from 'react-native-elements';
+import { Avatar, ListItem, Icon } from 'react-native-elements';
 import { baseUrl } from "../shared/baseUrl";
 // import { CAMPSITES } from "../shared/CAMPSITES";
 
@@ -25,6 +25,11 @@ const CampsitesScreen = ({ navigation }) => {
                         {campsite.location}
                     </ListItem.Subtitle>
                 </ListItem.Content>
+                <Icon 
+                    name={campsite.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#da0e0e'
+                />
             </ListItem>
         )
     };
